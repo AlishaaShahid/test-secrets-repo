6918b75ba9fb4d36e8128c8cc9965fc326f9207a
AlishaaShahid <alishashahidkhan77@gmail.com>
2025-10-05 07:21:11 +0500

    Add fake secrets for scanner testing

---
+++ secret_example.c
@@ -0,0 +1,7 @@
+/* sample C file with fake secret in comment */ 
+int main() { 
+    // API_KEY: FAKE_C_COMMENT_KEY_111 
+    char *key = "FAKE_C_STRING_KEY_222"; 
+    printf("Hello\\n"); 
+    return 0; 
+} 
