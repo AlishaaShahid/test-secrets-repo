a0e52e601f5bc32af5cf88e88a11d7559b481ca7
AlishaShahid <136807533+AlishaaShahid@users.noreply.github.com>
2025-10-05 07:28:34 +0500

    Create fake_keys_test.c

---
+++ fake_keys_test.c
@@ -0,0 +1,14 @@
+// Fake AWS Key
+const char* aws_key = "AKIA1234567890FAKEKEY";
+
+// Fake Slack Token
+const char* slack_token = "xoxb-123456789012-ABCDEFGHIJKLMNOPQRST";
+
+// Fake GitHub PAT
+const char* github_pat = "ghp_FAKE1234567890abcdefghijklmnop";
+
+// Fake JWT
+const char* jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.fakePayload.fakeSignature";
+
+// Fake high-entropy string
+const char* random_key = "8lksd923jl29dkl39dkas03jkdkls9203";
